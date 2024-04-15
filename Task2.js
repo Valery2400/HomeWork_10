5338241cf4b020e0e88050240ab1a9587bf9b988
Valery2400 <valeryk@hotmail.com>
2024-04-15 20:05:33 +0200

    Final version

--- Task2.js
+++ Task2.js
@@ -14,32 +14,30 @@
 // Подсказка: удобно использовать в одном из методов switch-case:
 
 const weatherForecast = (weatherCode) => {
-    
-    const decode =(weatherCode)=>{
-        
-    switch (weatherCode) {
-        case SQ:
-            console.log("шквал");
-        case PO:
-            console.log("пыльный вихрь");
-        case FC:
-            console.log("торнадо");
-        case BR:
-            console.log("дымка (видимость от 1 до 9 км)");
-        case HZ:
-            console.log("мгла (видимость менее 10 км)");
-        case FU:
-            console.log("дым (видимость менее 10 км)");
-        case DS:
-            console.log("пыльная буря (видимость менее 10 км)");
-        case SS:
-            console.log("песчаная буря (видимость менее 10 км)");
+           const decode = (weatherCode) =>{
+            switch (weatherCode) {
+        case "SQ":
+            return "шквал";
+        case "PO":
+            return "пыльный вихрь";
+        case "FC":
+            return "торнадо";
+        case "BR":
+            return "дымка (видимость от 1 до 9 км)";
+        case "HZ":
+            return "мгла (видимость менее 10 км)";
+        case "FU":
+            return "дым (видимость менее 10 км)";
+        case "DS":
+            return "пыльная буря (видимость менее 10 км)";
+        case "SS":
+            return "песчаная буря (видимость менее 10 км)";
          default:
-            console.log("неизвестный код или замечательная погода");
+            return"неизвестный код или замечательная погода";
         }
 
     }
-        return decode;
+        return decode(weatherCode);
 
 }
-weatherForecast("77")
+console.log(weatherForecast("77"));
